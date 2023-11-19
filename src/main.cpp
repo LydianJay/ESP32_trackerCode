dedfce5ab11af7ee0d045c29f284df6409aa1cc4
LydianJay <lloydjayedradan@gmail.com>
2023-11-20 00:36:05 +0800

    added blinking feature

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
   MIT License
 
-  Copyright (c) 2022 LydianJay (Lloyd Jay Arpilleda Edradan)
+  Copyright (c) 2023 LydianJay (Lloyd Jay Arpilleda Edradan)
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
